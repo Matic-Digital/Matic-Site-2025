feb15f648b2c24cf4700ac2f49ac300e94d41852
Stilian-matic <stilian@maticdigital.com>
2025-03-26 10:15:17 -0600

    added google and clutch to cors for live updates fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,12 +8,12 @@ export function middleware(request: NextRequest) {
   // Set Content Security Policy headers to allow Contentful resources and other third-party services
   const contentSecurityPolicy = `
     default-src 'self';
-    script-src 'self' 'unsafe-inline' 'unsafe-eval' unpkg.com static.contentful.com *.contentful.com o2239.ingest.sentry.io *.sentry.io cmp.osano.com munchkin.marketo.net *.loom.com cdn.loom.com evs.sgmt.loom.com widget.clutch.co;
-    style-src 'self' 'unsafe-inline' *.contentful.com *.loom.com cdn.loom.com;
-    img-src 'self' data: images.ctfassets.net *.contentful.com *.loom.com cdn.loom.com chrome-extension:;
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' unpkg.com *.unpkg.com static.contentful.com *.contentful.com o2239.ingest.sentry.io *.sentry.io cmp.osano.com munchkin.marketo.net *.loom.com cdn.loom.com evs.sgmt.loom.com widget.clutch.co *.googletagmanager.com www.googletagmanager.com;
+    style-src 'self' 'unsafe-inline' *.contentful.com *.loom.com cdn.loom.com unpkg.com *.unpkg.com;
+    img-src 'self' data: images.ctfassets.net *.contentful.com *.loom.com cdn.loom.com chrome-extension: *.googletagmanager.com www.google-analytics.com;
     font-src 'self' *.contentful.com *.loom.com cdn.loom.com data:;
-    connect-src 'self' api.contentful.com cdn.contentful.com preview.contentful.com graphql.contentful.com *.contentful.com o2239.ingest.sentry.io *.sentry.io *.loom.com evs.sgmt.loom.com;
-    frame-src 'self' *.contentful.com *.loom.com www.loom.com;
+    connect-src 'self' api.contentful.com cdn.contentful.com preview.contentful.com graphql.contentful.com *.contentful.com o2239.ingest.sentry.io *.sentry.io *.loom.com evs.sgmt.loom.com unpkg.com *.unpkg.com *.google-analytics.com *.analytics.google.com;
+    frame-src 'self' *.contentful.com *.loom.com www.loom.com widget.clutch.co;
     worker-src 'self' blob:;
     media-src 'self' *.loom.com cdn.loom.com;
     frame-ancestors 'self' https://app.contentful.com;
