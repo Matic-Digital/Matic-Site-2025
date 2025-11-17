0db31e764e99ae5b27b40bd18241e52697b27562
Matt-Morgan-7 <matt@morgans4.com>
2025-11-17 15:48:16 -0700

    added form webhook to content security policy

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,7 +12,7 @@ export function middleware(request: NextRequest) {
     style-src 'self' 'unsafe-inline' *.contentful.com *.loom.com cdn.loom.com unpkg.com *.unpkg.com;
     img-src 'self' data: images.ctfassets.net videos.ctfassets.net *.contentful.com *.loom.com cdn.loom.com chrome-extension: *.googletagmanager.com www.google-analytics.com;
     font-src 'self' *.contentful.com *.loom.com cdn.loom.com data:;
-    connect-src 'self' api.contentful.com cdn.contentful.com preview.contentful.com graphql.contentful.com *.contentful.com *.ctfassets.net assets.ctfassets.net downloads.ctfassets.net o2239.ingest.sentry.io *.sentry.io *.loom.com evs.sgmt.loom.com unpkg.com *.unpkg.com *.google-analytics.com *.analytics.google.com *.lottie.host lottie.host *.execute-api.us-west-2.amazonaws.com;
+    connect-src 'self' api.contentful.com cdn.contentful.com preview.contentful.com graphql.contentful.com *.contentful.com *.ctfassets.net assets.ctfassets.net downloads.ctfassets.net o2239.ingest.sentry.io *.sentry.io *.loom.com evs.sgmt.loom.com unpkg.com *.unpkg.com *.google-analytics.com *.analytics.google.com *.lottie.host lottie.host *.execute-api.us-west-2.amazonaws.com hooks.zapier.com;
     frame-src 'self' *.contentful.com *.loom.com www.loom.com widget.clutch.co;
     worker-src 'self' blob:;
     media-src 'self' *.loom.com cdn.loom.com *.ctfassets.net videos.ctfassets.net;
